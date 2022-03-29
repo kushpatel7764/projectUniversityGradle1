f46e18961283c591da3175fac2f4e02516352280
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-03-29 11:13:11 -0400

    Update Professor.java

--- src/main/java/bsu/comp152/Professor.java
+++ src/main/java/bsu/comp152/Professor.java
@@ -3,13 +3,21 @@ package bsu.comp152;
 import java.util.ArrayList;
 
 public class Professor{
+    ArrayList<Student> adviseeList;
+    String name;
+    String department;
     public Professor(String name, String department) {
+        this.name = name;
+        this.department = department;
+        adviseeList = new ArrayList<Student>();
     }
 
     public void addAdvisee(Student advisee) {
+        adviseeList.add(advisee);
     }
 
     public void removeAdvisee(Student advisee) {
+        adviseeList.remove(advisee);
     }
 
     public ArrayList<Student> getAdviseeList() {
