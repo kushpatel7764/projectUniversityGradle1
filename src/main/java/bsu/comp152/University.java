67497b9c386307c8d020938a89e1c8f7012642bc
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-03-29 12:10:53 -0400

    Update University.java

--- src/main/java/bsu/comp152/University.java
+++ src/main/java/bsu/comp152/University.java
@@ -4,7 +4,14 @@ import java.util.*;
 
 public class University {
 
+    ArrayList<Student> studentList;
+    int nextStudentID;
+    ArrayList<Professor> faculty;
+
     public University() {
+        studentList = new ArrayList<Student>();
+        faculty = new ArrayList<Professor>();
+        nextStudentID = 2000;
     }
 
     public ArrayList<Professor> getFaculty() {
@@ -12,17 +19,26 @@ public class University {
     }
 
     public void hireProfessor(Professor professor) {
+        faculty.add(professor);
     }
 
     public void admitStudent(Student student) {
+        studentList.add(student);
+        nextStudentID++;
     }
 
     public int getNextStudentID() {
-        return 0;
+        return nextStudentID;
     }
 
     public ArrayList<Student> findStudent(int id) {
-        return null;
+        ArrayList<Student> foundStudent = new ArrayList<Student>();
+        for (int i = 0; i < studentList.size(); i++){
+            if (studentList.get(i).studentID == id){
+                foundStudent.add(studentList.get(i));
+            }
+        }
+        return foundStudent;
     }
 
     public List<Student> graduateStudents() {
