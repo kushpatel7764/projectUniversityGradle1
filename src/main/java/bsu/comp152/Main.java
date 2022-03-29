7ccbbfbc6400381e8ea204a84c789bcf9196b701
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-03-29 00:38:45 -0400

    Update Main.java

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,5 +1,11 @@
 package bsu.comp152;
 public class Main {
     public static void main(String[] args) {
+        Professor p1 = new Professor("Jessi", "Science");
+        Student s1 = new Student("Kush", p1, 1);
+        s1.addClassToTranscript(4,4.0);
+        System.out.println(s1.toString());
+        s1.addClassToTranscript(8, 2.0);
+        System.out.println(s1.toString());
     }
 }
