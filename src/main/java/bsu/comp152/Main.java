8f95c72e37885309dc2661c4a1191764dd93e1de
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-03-30 15:22:08 -0400

    Update Main.java

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,11 +1,26 @@
 package bsu.comp152;
 public class Main {
     public static void main(String[] args) {
+       //Student
+        System.out.println("------Student------");
         Professor p1 = new Professor("Jessi", "Science");
         Student s1 = new Student("Kush", p1, 1);
+        Student s2 = new Student("Chris", p1, 2);
+        Student s3 = new Student("Steve", p1, 3);
         s1.addClassToTranscript(4,4.0);
         System.out.println(s1.toString());
         s1.addClassToTranscript(8, 2.0);
         System.out.println(s1.toString());
+        // Professor
+        System.out.println("------Professor------");
+        p1.addAdvisee(s1);
+        p1.addAdvisee(s2);
+        p1.addAdvisee(s3);
+        System.out.println(p1);
+        p1.removeAdvisee(s1);
+        System.out.println(p1);
+        p1.addAdvisee(s1);
+        p1.removeAdvisee(s2);
+        System.out.println(p1);
     }
 }
