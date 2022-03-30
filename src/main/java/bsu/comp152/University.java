45696e715eb21b98b70b84a8b291493263c24214
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-03-29 23:56:18 -0400

    Update University.java

--- src/main/java/bsu/comp152/University.java
+++ src/main/java/bsu/comp152/University.java
@@ -55,6 +55,26 @@ public class University {
         }
         return graduatedStudents;
     }
+     /*
+//Having a hard time with this.
+public List<Student> graduateStudents() {
+    int listSize = studentList.size();
+    ArrayList<Student> graduatedStudents = new ArrayList<Student>();
+    //if(studentList.size() == 0) return graduatedStudents;
+    for (int i = listSize-1; i >=0; i--) {
+        Student currentStudent = studentList.get(i);
+        if (currentStudent.getGPA() >= 2.0 && currentStudent.getCreditHours() >= 20) {
+
+            graduatedStudents.add(currentStudent);
+            studentList.remove(currentStudent);
+            currentStudent.getAdvisor().removeAdvisee(currentStudent);
+
+        }
+
+    }
+    return graduatedStudents;
+}
+    */
 
 }
 
