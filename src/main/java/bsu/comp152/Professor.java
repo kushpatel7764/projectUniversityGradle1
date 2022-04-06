5af659841ea019243a492c0cb084b6ff1ef6ca67
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-04-06 13:32:30 -0400

    Update Professor.java

--- src/main/java/bsu/comp152/Professor.java
+++ src/main/java/bsu/comp152/Professor.java
@@ -2,14 +2,13 @@ package bsu.comp152;
 
 import java.util.ArrayList;
 
-public class Professor{
-    ArrayList<Student> adviseeList;
+public class Professor{  ArrayList<Student> adviseeList;
     String name;
     String department;
     public Professor(String name, String department) {
         this.name = name;
         this.department = department;
-        adviseeList = new ArrayList<Student>();
+        adviseeList = new ArrayList<>();
     }
 
     public void addAdvisee(Student advisee) {
@@ -31,7 +30,7 @@ public class Professor{
     public String toString() {
         String output = "";
         for (int i = 0; i < adviseeList.size(); i++){
-            output += adviseeList.get(i).name + " ";
+            output += adviseeList.get(i).getName() + " ";
         }
         return "Professor's name: " + name + "\n" + "Department: " + department + "\n" + "Advisee List: " + output;
     }
